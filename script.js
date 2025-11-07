7b1e5b7d35bd739b50a3efc700cd09af3dc09dd8
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2025-11-07 22:37:09 +0100

    l'ajoute local storage sans logique

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 
 let toutsSections = document.querySelectorAll('.screen');
 let btns = document.querySelectorAll('.sidebar__btn');
-
+const headerr=document.getElementById('page-title');
 function screenlesSection(index) {
     toutsSections.forEach(section => {
         section.classList.remove('is-visible')
         const datasection = index.dataset.screen;
-        if (section.dataset.screen === datasection) {
+        let sectionstatus=section.dataset.screen;
+        if (sectionstatus === datasection) {
             section.classList.add('is-visible')
-        }
-    });
 
+            //localStorage
+            localStorage.setItem('datasection',sectionstatus);
+        }
+
+    });
     btns.forEach(btn => {
         btn.classList.remove("is-active")
         const databtns = index.dataset.screen;
-        if (btn.dataset.screen === databtns) {
+        let btnstatus=btn.dataset.screen;
+        if (btnstatus === databtns) {
             btn.classList.add("is-active");
+            //localStorage
+            localStorage.setItem('btnstatus',databtns);
         }
+
+       
     });
+
 }
+
+
+
+
+
+
+
+
+
+
+
 
