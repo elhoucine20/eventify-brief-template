93fe11c1e605e949ee94c1f26c286dd67358bbc7
elhoucine20 <elhoucinenaitbrahim@gmail.com>
2025-11-12 20:58:10 +0100

    EV-8#done:affichage de l'archive

---
+++ script.js
@@ -0,0 +1,275 @@
+
+let events = [];
+let archeve = [];
+// switch entre les buttons et les sections
+let toutsSections = document.querySelectorAll('.screen');
+let btns = document.querySelectorAll('.sidebar__btn');
+const title = document.getElementById('page-title');
+function screenlesSection(index) {
+  toutsSections.forEach(section => {
+    section.classList.remove('is-visible')
+    let datasection = index.dataset.screen;
+    let sectionstatus = section.dataset.screen;
+    if (sectionstatus === datasection) {
+      section.classList.add('is-visible')
+      //localStorage
+      localStorage.setItem('datasection', sectionstatus);
+    }
+
+  });
+  btns.forEach(btn => {
+    btn.classList.remove("is-active")
+    const databtns = index.dataset.screen;
+    let btnstatus = btn.dataset.screen;
+    if (btnstatus === databtns) {
+      btn.classList.add("is-active");
+      //localStorage
+      localStorage.setItem('btnstatus', databtns);
+    }
+  });
+
+  // verification de title
+  if (index.dataset.screen === "stats") {
+    title.textContent = "statistique";
+    // subtitle.textContent = "Overview of your events";
+  } else if (index.dataset.screen === "add") {
+    title.textContent = "Add event";
+    // subtitle.textContent = "Overview of your events";
+
+  } else if (index.dataset.screen === "list") {
+    title.textContent = "Events";
+    // subtitle.textContent = "Overview of your events";
+
+  } else if (index.dataset.screen === "archive") {
+    title.textContent = "Archive";
+    // subtitle.textContent = "Overview of your events";
+
+  }
+}
+
+// validation du form
+
+let form = document.querySelector("form");
+let btncreateevent = document.querySelector(".btn--primary");
+let btnclear = document.querySelector(".btn--ghost");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const regextitle = /^[a-zA-ZÀ-ÿ0-9\s'.,-]{2,100}$/;
+  const regeximageurl = /^(https?:\/\/.*.(?:png|jpg|jpeg|gif|webp|svg))$/i;
+  const regexdescription = /^[\wÀ-ÿ\s.,'?!-]{10,500}$/;
+  const regexnombrseats = /^[1-9][0-9]{0,2}$/;
+  const regexprixbase = /^(?:\d+|\d+.\d{1,2})$/;
+  const title = document.querySelector("#event-title").value;
+  const imageurl = document.querySelector("#event-image").value;
+  const description = document.querySelector("#event-description").value;
+  const nombrseats = document.querySelector("#event-seats").value;
+  const prixbase = document.querySelector("#event-price").value;
+  if (!regextitle.test(title)) {
+    alert("saisir title");
+  }
+
+  if (!regeximageurl.test(imageurl)) {
+    alert("saisir url");
+  }
+
+  if (!regexdescription.test(description)) {
+    alert("saisir description");
+  }
+
+
+  if (!regexnombrseats.test(nombrseats)) {
+    alert("saisir nombre seats");
+  }
+
+  if (!regexprixbase.test(prixbase)) {
+    alert("saisir le prix");
+  }
+  // stocke data
+  let Dataa = {
+    title: title,
+    imagee: imageurl,
+    discription: description,
+    seatss: nombrseats,
+    prixx: prixbase,
+  }
+  // console.log(Dataa);
+  events.push(Dataa);
+  // console.log(events);
+  listevents();
+  form.reset();
+});
+
+
+
+// add total(event,seats,prix)
+
+let totalevent = document.getElementById('stat-total-events');
+let toatlseats = document.getElementById('stat-total-seats');
+let totalrevenu = document.getElementById('stat-total-price');
+let TOTALEVENTS = Number(totalevent.textContent);
+let TOTALSEATS = Number(toatlseats.textContent);
+totalrevenu.innerHTML = "0";
+let TOTALREVENU = Number(totalrevenu.textContent);
+// input seats
+let seats = document.getElementById('event-seats');
+let price = document.getElementById('event-price');
+// alert("add event 1");
+function Statistique(){
+  btncreateevent.addEventListener('click', function () {
+
+  // events
+  ++TOTALEVENTS;
+  totalevent.innerHTML = TOTALEVENTS;
+  // seats
+  TOTALSEATS += Number(seats.value);
+  toatlseats.innerHTML = TOTALSEATS;
+  // revenu
+  TOTALREVENU += Number(price.value);
+  totalrevenu.innerHTML =TOTALREVENU;
+  // if(DELETEevent){
+  //   var newTotalEvent= Number(totalevent.textContent);
+  //   var newToatlSeats=Number(toatlseats.textContent);
+  //   var newTotalRevenu=Number(totalrevenu.textContent);
+    
+  // }
+ 
+});
+}
+     Statistique();
+
+
+// console.log('detals0');
+
+// list des events 
+function listevents() {
+  const tbody = document.querySelector(".table__body");
+  var cntrevent = 0;
+  tbody.innerHTML = "";
+  events.forEach(event => {
+    tbody.innerHTML += `
+    <tr class="table__row" >
+        <td>${++cntrevent}</td>
+        <td>${event.title}</td>
+        <td>${event.seatss}</td>
+        <td><span class="badge">$${event.prixx}</span></td>
+        <td>${contrVaraint}</td>
+
+        <td>
+          <button class="btn btn--small" data-action="details" onclick="Details(${cntrevent})"  data-event-id="1">Details</button>
+          <button class="btn btn--small" data-action="edit" data-event-id="1">Edit</button>
+          <button class="btn btn--danger btn--small" data-action="archive" class="deletet" data-event-id="1" onclick="DELETEevent(${cntrevent}),listeArchive()" >Delete</button>
+        </td>
+    </tr>`
+  });
+}
+// console.log('detals1');
+// details
+const modal = document.querySelector('.modal');
+function Details(index) {
+  modal.classList.remove('is-hidden');
+  const modal_body = document.querySelector('.modal__body');
+  const evv=events[index-1];
+  // events.forEach(event => {
+    modal_body.innerHTML = `
+  <h1>TITLE :${evv.title}</h1>
+  <p>description:${evv.discription}</p>
+  <p>seats: ${evv.seatss}</p>
+  <p>price: ${evv.prixx}$</p>
+`
+  // })
+}
+// console.log('detals10');
+
+
+// close modal
+function closemodal() {
+  const modaldiv = document.querySelector(".modal")
+  modal.classList.add('is-hidden');
+}
+
+/// add varaint
+let contrVaraint = 0;
+function ListVaraint() {
+  contrVaraint++;
+  const varaintList = document.querySelector('.variants__list');
+  varaintList.innerHTML += `
+<div class="variant-row">
+   <input type="text" class="input variant-row__name" placeholder="Variant name (e.g., 'Early Bird')" />
+   <input type="number" class="input variant-row__qty" placeholder="Qty" min="1" />
+   <input type="number" class="input variant-row__value" placeholder="Value" step="0.01" />
+   <select class="select variant-row__type">
+   <option value="fixed">Fixed Price</option>
+   <option value="percentage">Percentage Off</option>
+   </select>
+   <button  type="button" class="btn btn--danger btn--small variant-row__remove" onclick="removebtnDIV(this)">Remove</button>
+</div>
+`
+}
+// console.log(events);
+
+//   btn remove varaint
+function removebtnDIV(btn) {
+  contrVaraint--;
+  let BTNREMOVE = btn.closest('.variant-row');
+  BTNREMOVE.remove();
+}
+// delete event
+function DELETEevent(btn) {
+  // const delett = btn.closest('.table__row');
+  const index=btn-1;
+  // const id=delett.dataset.eventId;
+  // const index=events.findIndex(e=>e.id===id);
+  var  removed=events.splice(index,1)[0];
+  archeve.push(removed);
+  // delett.remove();
+  listeArchive();  
+  listevents();
+  // new total events
+  --TOTALEVENTS;
+  totalevent.innerHTML=TOTALEVENTS;
+  if(TOTALEVENTS===0){
+     // seats
+  TOTALSEATS = 0;
+  toatlseats.innerHTML = TOTALSEATS;
+  // revenu
+  TOTALREVENU = 0;
+  totalrevenu.innerHTML =TOTALREVENU;
+  }
+
+}
+// lestArchive
+function listeArchive() {
+  const tbody = document.querySelectorAll(".table__body")[1];
+  // if(!tbody) return;
+  let cntrevent = 0;
+  tbody.innerHTML = "";
+  archeve.forEach(event => {
+    tbody.innerHTML += `
+         <tr class="table__row" >
+               <td>${++cntrevent}</td>
+               <td>${event.title}</td>
+               <td>${event.seatss}</td>
+              <td>${event.prixx}$</td>
+              <td>${event.contrVaraint || 0}</td>
+              <td>
+                <button class="btn btn--small" data-action="restore" onclick="restorEvent(${cntrevent})" data-event-id="1">Restore</button>
+               </td>
+         </tr>`
+  });
+}
+
+// restor event
+function restorEvent(btnn){
+  //  const indexx = btn.closest('.table__row');
+  const index=btnn - 1;
+  // const id=indexx.dataset.eventId;
+  // const index=events.findIndex(e=>e.id===id);
+  const  restored =archeve.splice(index, 1)[0];
+  events.push(restored);
+  // restor.remove();
+  // console.log(removedd);
+  listeArchive(); 
+  listevents();
+}
+
+
